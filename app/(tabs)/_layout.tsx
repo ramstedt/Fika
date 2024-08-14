4830d207e7ab507f541710c23e0e33ebd0f5e26a
Emma Ramstedt <ceramstedt@gmail.com>
2024-08-14 09:16:33 +0200

    wrap session logic to _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,36 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import React, { useEffect, useState, ReactNode } from 'react';
+import { View, Text } from 'react-native';
+import { supabase } from '@/lib/supabase'; 
+import { Session } from '@supabase/supabase-js';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+type LayoutProps = {
+  children: ReactNode; 
+};
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function Layout({ children }: LayoutProps) {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+
+    return () => {
+      authListener?.subscription?.unsubscribe();
+    };
+  }, []);
+
+  if (!session) {
+    return <Text>Please log in to view this content.</Text>;
+  }
+
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <View>
+      {/* content here */}
+      {children}
+    </View>
   );
 }
